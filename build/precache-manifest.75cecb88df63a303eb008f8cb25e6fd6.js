88b35183512a981d55655735b58f6c067c5c5162
tendainy <tendai.nyandoro@platform45.com>
2022-08-20 22:39:28 +0200

    redeploy to gh

---
+++ build/precache-manifest.75cecb88df63a303eb008f8cb25e6fd6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "2238ce616eb603fb5306",
+    "url": "/tnyandoro/inexamed-ddi-checker/static/css/main.031bbec3.chunk.css"
+  },
+  {
+    "revision": "2238ce616eb603fb5306",
+    "url": "/tnyandoro/inexamed-ddi-checker/static/js/main.2238ce61.chunk.js"
+  },
+  {
+    "revision": "9f6f6cec55adb720d38c",
+    "url": "/tnyandoro/inexamed-ddi-checker/static/js/1.9f6f6cec.chunk.js"
+  },
+  {
+    "revision": "5dec50a67f1dda7242bb",
+    "url": "/tnyandoro/inexamed-ddi-checker/static/js/2.5dec50a6.chunk.js"
+  },
+  {
+    "revision": "0c7a4e34dd97afeead03",
+    "url": "/tnyandoro/inexamed-ddi-checker/static/js/runtime~main.0c7a4e34.js"
+  },
+  {
+    "revision": "9de2b22ee63f4384554ad5e5fd9860d7",
+    "url": "/tnyandoro/inexamed-ddi-checker/static/media/bg.9de2b22e.jpeg"
+  },
+  {
+    "revision": "aadc42af59d59fdb4d95c208e5badeba",
+    "url": "/tnyandoro/inexamed-ddi-checker/static/media/header-bg-small.aadc42af.jpg"
+  },
+  {
+    "revision": "c16be79750c506514730f581d5338223",
+    "url": "/tnyandoro/inexamed-ddi-checker/static/media/close-icon.c16be797.svg"
+  },
+  {
+    "revision": "32f069c82b8ef9894b838de1f99107e9",
+    "url": "/tnyandoro/inexamed-ddi-checker/index.html"
+  }
+];
