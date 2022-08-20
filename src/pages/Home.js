7ad636425cf8e1f97a8e5d8332826a9acd571092
tendainy <tendai.nyandoro@platform45.com>
2022-08-20 11:06:09 +0200

    Style the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import Button from "../UI/Button"
 
 export default function Home() {
   return (
-    <div>
+    <div className="main">
       <h1>Welcome to Inexamed Drug drug interaction Checker</h1>
       <Button type="submit">Let Get Started</Button>
     </div>
